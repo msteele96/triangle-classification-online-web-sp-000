65eb499915a9576aac7b8f0cf912d77461c3f914
Marc Steele <marcsteele2014@gmail.com>
2020-11-01 20:08:38 +0000

    Automatically backed up by Learn

--- lib/triangle.rb
+++ lib/triangle.rb
@@ -9,7 +9,7 @@ class Triangle
   end
 
   def kind
-    if @side_1 = 0 && @side_2 = 0 && @side_3 = 0
+    if @side_1 = 0 || @side_2 = 0 || @side_3 = 0
       begin
         raise TriangleError
       rescue TriangleError => error
